3099fe66eeecdb0574211b7787a655a516231924
Tushar <111505346+Tushar6396@users.noreply.github.com>
2023-07-29 13:33:25 +0530

    completed add-to-cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -40,11 +40,19 @@ function ProductScreen(props) {
     fetchData();
   }, [slug]);
 
-  const { dispatch: ctxDispatch } = useContext(Store);
-  const addToCartHandler = () => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry, The Product is out of Stock');
+      return;
+    }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
 
